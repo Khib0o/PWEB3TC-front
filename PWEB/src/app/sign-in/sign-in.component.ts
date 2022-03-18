c4a4e3b550f48177fdc24a18499a910d5e06f6ce
sontonyD <sontony.dinh@gmail.com>
2022-03-18 06:51:10 +0100

    Connexion avec compte google

--- PWEB/src/app/sign-in/sign-in.component.ts
+++ PWEB/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 
@@ -14,6 +14,8 @@ export class SignInComponent implements OnInit {
     email : '',
     password : ''
   }
+  auth2: any;
+  @ViewChild('loginRef', {static: true }) loginElement!: ElementRef;
   /*
   registerUserData = {
     email: '',
@@ -32,6 +34,56 @@ export class SignInComponent implements OnInit {
   constructor(private _auth: AuthService) { }
 
   ngOnInit(): void {
+    this.googleAuthSDK();
+  }
+
+  callLoginButton() {
+     
+    this.auth2.attachClickHandler(this.loginElement.nativeElement, {},
+      (googleAuthUser:any) => {
+     
+        let profile = googleAuthUser.getBasicProfile();
+        console.log('Token || ' + googleAuthUser.getAuthResponse().id_token);
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+            
+       /* Write Your Code Here */
+    
+      }, (error:any) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+ 
+  }
+  
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  googleAuthSDK() {
+     
+    (<any>window)['googleSDKLoaded'] = () => {
+      (<any>window)['gapi'].load('auth2', () => {
+        this.auth2 = (<any>window)['gapi'].auth2.init({
+          client_id: '54005535557-ebru8k3h57c5qd8hp1lrqopkajvuh670.apps.googleusercontent.com',
+          cookiepolicy: 'single_host_origin',
+          scope: 'profile email'
+        });
+        this.callLoginButton();
+      });
+    }
+     
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement('script'); 
+      js.id = id;
+      js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
+      fjs?.parentNode?.insertBefore(js, fjs);
+    }(document, 'script', 'google-jssdk'));
+   
   }
 
   loginUser() {
